d6a58d660258fc646e6169dd0c2fd3b4d0072ca5
Gamemon <33440226+Gamemon@users.noreply.github.com>
2024-01-09 14:27:24 -0600

    Update CheckersGamev2.java
    Added another 2d array that'll be rewritten for allowed moves, started input detection reading for force jump and regular move detection

--- CheckersGamev2.java
+++ CheckersGamev2.java
@@ -13,6 +13,12 @@ import java.awt.*;
 
 public class CheckersGamev2 {
 
+    static int countInp = 0;
+    static String initialLoc = "00";
+    static String finalLoc = "00";
+    static String indexIL = "";
+    static String indexFL = "";
+    static boolean player1 = true;
     static GameBoard gamey = new GameBoard();
     static String[][] chips = {
 {"", "BC", "", "BC", "", "BC", "", "BC"}, 
@@ -34,12 +40,75 @@ public class CheckersGamev2 {
 {gamey.b49, gamey.b50, gamey.b51, gamey.b52, gamey.b53, gamey.b54, gamey.b55, gamey.b56},
 {gamey.b57, gamey.b58, gamey.b59, gamey.b60, gamey.b61, gamey.b62, gamey.b63, gamey.b64},
 };
+    static boolean[][] boos = {
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false},
+    {false, false, false, false, false, false, false, false}
+    };
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         gamey.setVisible(true);
         
         
-        //Initialize gameboard -first testing and then added in own method for updating gameboard
+        //Initialize gameboard -first testing and then added in own method for updating gameboard - DONE
+        gameBoardUpdate();
+        
+        
+    }
+    
+    public static void movementStart(){
+        //First needs to check if chip selected is active and on the current players team
+        int tempCount = 0;
+        for (int i = 0; i < chips.length; i++){ //Change the location nums into indexes (String form, "firstIndexLastIndex")
+            for (int j = 0; j < chips[i].length; j++){
+                tempCount++;
+                if (tempCount == Integer.parseInt(initialLoc)){
+                    indexIL = i + j + "";
+                }
+                if (tempCount == Integer.parseInt(finalLoc)){
+                    indexFL = i + j + "";
+                }
+            }
+        }
+        int IL1 = Integer.parseInt(indexIL.substring(0,1));
+        int IL2 = Integer.parseInt(indexIL.substring(1,2));
+        int FL1 = Integer.parseInt(indexFL.substring(1,2));
+        int FL2 = Integer.parseInt(indexFL.substring(1,2)); //noice
+        //Should determine where the chip is and then allow only specific movements from that given location - NEW 2d ARRAY NEEDED
+        if (player1){
+            if (chips[IL1][IL2].substring(0, 1).equals("R")){ //This sucks to look at - Fixed
+                //Check if it's a king or not, then detect if theres any force jumps
+                //If no force jump do regular forward/backward moves (depending on type)
+                
+            }
+        }
+    }
+    
+    public static void inputDetect(String loc){ //Locations will be button numbers rather than index locations... cause thats a lot of work
+        countInp++;
+        if (countInp == 1){ //Take the location and assign it to initial or final locations based on if it's first or last input
+            initialLoc = loc;
+        } else if (countInp == 2){
+            finalLoc = loc;
+            movementStart();
+        } else {
+            countInp = 0;
+            player1 = !player1; //Next player! although itll still switch player even if invalid input, oh well NEED TO FIX****************************************************
+        }
+    }
+    
+    public static void diagonalJumps(){
+        
+    }
+    
+    /*
+    No parameters needed, just a simple updating of the visuals using both static 2d arrays of buttons and strings
+    */
+    public static void gameBoardUpdate(){
         for (int i = 0; i < chips.length; i++){
             for (int j = 0; j < chips[i].length; j++){
                 if (chips[i][j].equals("")){ //No chip
