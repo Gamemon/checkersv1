6195e18e73f781c5d4b199b377cff2be2eaf88d7
Gamemon <33440226+Gamemon@users.noreply.github.com>
2024-01-09 07:55:44 -0600

    Added the main files for the gameboard
    Gameboard creation successful - each button displays text and color
    Logic is pretty much everything left

---
+++ CheckersGamev2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.checkersgamev2;
+import javax.swing.*;
+/**
+ *
+ * @author antho
+ */
+public class CheckersGamev2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        GameBoard gamey = new GameBoard();
+        gamey.setVisible(true);
+    }
+}
