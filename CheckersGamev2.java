cfca6fdae1111740d7f2cbd06f1b0a3e788eef95
Gamemon <33440226+Gamemon@users.noreply.github.com>
2024-01-09 08:25:39 -0600

    Update CheckersGamev2.java
    Both 2d arrays are now functional - aligned indexes with one another

--- CheckersGamev2.java
+++ CheckersGamev2.java
@@ -12,7 +12,8 @@ import javax.swing.*;
 
 public class CheckersGamev2 {
 
-    String[][] chips = {
+    static GameBoard gamey = new GameBoard();
+    static String[][] chips = {
 {"", "BC", "", "BC", "", "BC", "", "BC"}, 
 {"BC", "", "BC", "", "BC", "", "BC", ""}, 
 {"", "BC", "", "BC", "", "BC", "", "BC"},
@@ -22,12 +23,26 @@ public class CheckersGamev2 {
 {"", "RC", "", "RC", "", "RC", "", "RC"},
 {"RC", "", "RC", "", "RC", "", "RC", ""},
 };
+    static javax.swing.JButton[][] butts = {
+{gamey.b1, gamey.b2, gamey.b3, gamey.b4, gamey.b5, gamey.b6, gamey.b7, gamey.b8},
+{gamey.b9, gamey.b10, gamey.b11, gamey.b12, gamey.b13, gamey.b14, gamey.b15, gamey.b16},
+{gamey.b17, gamey.b18, gamey.b19, gamey.b20, gamey.b21, gamey.b22, gamey.b23, gamey.b24},
+{gamey.b25, gamey.b26, gamey.b27, gamey.b28, gamey.b29, gamey.b30, gamey.b31, gamey.b32},
+{gamey.b33, gamey.b34, gamey.b35, gamey.b36, gamey.b37, gamey.b38, gamey.b39, gamey.b40},
+{gamey.b41, gamey.b42, gamey.b43, gamey.b44, gamey.b45, gamey.b46, gamey.b47, gamey.b48},
+{gamey.b49, gamey.b50, gamey.b51, gamey.b52, gamey.b53, gamey.b54, gamey.b55, gamey.b56},
+{gamey.b57, gamey.b58, gamey.b59, gamey.b60, gamey.b61, gamey.b62, gamey.b63, gamey.b64},
+};
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        GameBoard gamey = new GameBoard();
         gamey.setVisible(true);
         
         
         //Initialize gameboard
+        for (int i = 0; i < chips.length; i++){
+            for (int j = 0; j < chips[i].length; j++){
+                
+            }
+        }
     }
 }
